68e37316fa93e9ec200afaae6d8dd85480616b55
Gustavo Silva <a46048@alunos.ipb.pt>
2024-11-05 11:43:33 +0000

    atualizado para vercel

--- pinarq/tailwind.config.js
+++ pinarq/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./scr**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
