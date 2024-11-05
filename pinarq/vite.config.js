3007419a010882f368456b2492beab7f7bc7006d
Gustavo Silva <a46048@alunos.ipb.pt>
2024-11-05 11:49:34 +0000

    .

--- pinarq/vite.config.js
+++ pinarq/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vercel from '@vercel/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react()],
 
 })
